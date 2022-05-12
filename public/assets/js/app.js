cd983e44bc34f662571bcfd47ed0d22fa660bdf9
antoniodelhierro <delhierroantonio@icloud.com>
2022-05-12 17:26:19 -0500

    Include conditionals, functions, and a while loop

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,47 +1,73 @@
-let name = prompt('Hola bienvenido a ProPC porfavor ingrese su nombre para continuar:');
-let elegirPc = prompt(`Hola ${name}, Para realizar una compra primero eliga el modelo de pc que desea ingresando un numero: 1.Gama Baja - 2.Gama Media - 3.Gama Alta`);
-
-const juegosBaja = ['Counter Strike', 'Valorant', 'Fortnite'];
-const juegosMedia = ['GTA V', 'Hitman', 'Resident Evil'];
-const juegosAlta = ['Battlefield', 'Forza', 'War Zone'];
+let userName = prompt('Por favor ingrese su nombre');
+let userAge = parseInt(prompt('Por favor ingrese su edad'));
+let pcPrice;
 
 const pcBaja = {
-    procesador: 'Intel i3',
-    memoria: '4 GB',
-    placa: 'Asus Prime 660',
-    alacenamiento: '500GB',
-    video: 'RTX 3050',
-    precio: '$3500'
+    nombre: 'PC Gama Baja',
+    precio: 2000,
+    juegos: ['Counter Strike', 'Valorant', 'Fortnite']
 }
 const pcMedia = {
-    procesador: 'Intel i5',
-    memoria: '8 GB',
-    placa: 'Asus Prime 660',
-    alacenamiento: '1TB',
-    video: 'RTX 3070',
-    precio: '$6800'
+    nombre: 'PC Gama Media',
+    precio: 5000,
+    juegos: ['GTA V', 'Hitman', 'Resident Evil']
 }
 const pcAlta = {
-    procesador: 'Intel i7',
-    memoria: '16 GB',
-    placa: 'Asus Prime 660',
-    alacenamiento: '2TB',
-    video: 'RTX 3080',
-    precio: '$12000'
+    nombre: 'PC Gama Alta',
+    precio: 9000,
+    juegos: ['Battlefield', 'Forza', 'War Zone']
 }
 
-if (elegirPc == 1) {
-    elegirPc = 'pc Gama Baja';
-    alert('Ha elegido la Pc de Gama Baja, su pago esta siendo procesado..');
-    alert(`Usted ha comprado la pc: ${elegirPc} con un procesador ${pcBaja.procesador}, ${pcBaja.memoria} de memoria RAM, ${pcBaja.video} en tarjeta de video y podra jugar estos juegos: ${juegosBaja}. Todo esto por un total de: ${pcBaja.precio}, Gracias por su compra!`);
-} else if (elegirPc == 2) {
-    elegirPc = 'pc Gama Media';
-    alert('Ha elegido la Pc de Gama Media, su pago esta siendo procesado..');
-    alert(`Usted ha comprado la pc: ${elegirPc} con un procesador ${pcMedia.procesador}, ${pcMedia.memoria} de memoria RAM, ${pcMedia.video} en tarjeta de video y podra jugar estos juegos: ${juegosMedia}. Todo esto por un total de: ${pcMedia.precio}, Gracias por su compra!`);
-} else if (elegirPc == 3) {
-    elegirPc = 'pc Gama Alta';
-    alert('Ha elegido la Pc de Gama Alta, su pago esta siendo procesado..');
-    alert(`Usted ha comprado la pc: ${elegirPc} con un procesador ${pcAlta.procesador}, ${pcAlta.memoria} de memoria RAM, ${pcAlta.video} en tarjeta de video y podra jugar estos juegos: ${juegosAlta}. Todo esto por un total de: ${pcAlta.precio}, Gracias por su compra!`);
+if (userAge > 18) {
+    alert(`Bienvenido a ProPC ${userName} antes de proceder debe registrar una contraseña e iniciar sesion:`);
+    signIn();
 } else {
-    alert('porfavor seleccione un producto valido de nuestro catalogo!');
+    alert('Necesitas ser mayor de edad para realizar compras');
 }
+
+function signIn() {
+    const PASSWORD = prompt('Registre una contraseña:');
+    const LOGIN = prompt('Ahora ingrese su contraseña para acceder a la tienda');
+    if (LOGIN == PASSWORD && userAge > 18) {
+        alert('Contraseña correcta!');
+    } else {
+        alert('Contraseña incorrecta!');
+    }
+}
+
+let pcSelected = parseInt(prompt(`Bien ${userName} ahora eliga el modelo de pc que desea ingresando un numero: 1-Gama Baja ($2000) 2-Gama Media ($5000) 3-Gama Alta ($9000)`));
+
+switch (pcSelected) {
+    case 1:
+        pcPrice = pcBaja.precio;
+        console.log(pcPrice);
+        alert(`Ha seleccionado la PC de gama Baja, podra jugar juegos como: ${pcBaja.juegos}`);
+        break;
+    case 2:
+        pcPrice = pcMedia.precio;
+        console.log(pcPrice);
+        alert(`Ha seleccionado la PC de gama Media, podra jugar juegos como: ${pcMedia.juegos}`);
+        break;
+    case 3:
+        pcPrice = pcAlta.precio;
+        console.log(pcPrice);
+        alert(`Ha seleccionado la PC de gama Alta, podra jugar juegos como: ${pcAlta.juegos}`);
+        break;
+
+    default:
+        alert('Porfavor haga una seleccion validad');
+        break;
+}
+
+let amount = parseInt(prompt('Cuantas unidades desea comprar?'));
+
+function totalPrice() {
+    let checkout = pcPrice * amount;
+    while (amount != 0) {
+        alert(`El total de su compra sera: $${checkout}, por ${amount} PC's`);
+        break;
+    }
+}
+// main
+
+totalPrice();
